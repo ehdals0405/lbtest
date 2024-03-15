00d0bb2a93443e6e127db7a21752a6e4189370a7
origin <git@github.com:ehdals0405/PreProjectback.git>
2024-03-15 16:32:24 +0900

    test

---
+++ src/main/java/com/test/test1/IndexController.java
@@ -0,0 +1,20 @@
+package com.test.test1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+@Controller
+public class IndexController {
+    @GetMapping(value = {"/index.html", "/"})
+    public String index(Model model) throws UnknownHostException {
+        InetAddress ipAddress = InetAddress.getLocalHost();
+
+        model.addAttribute("ip", ipAddress.getHostAddress());
+        return "index";
+    }
+}
